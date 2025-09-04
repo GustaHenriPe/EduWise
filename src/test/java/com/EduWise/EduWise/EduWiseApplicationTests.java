59015465841a8421221008d5bc5c6b89eade1b4f
Gustavo Henrique Pereira Junqueira <guhhpj@gmail.com>
2025-09-04 10:45:37 -0300

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/test/java/com/EduWise/EduWise/EduWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EduWise.EduWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
