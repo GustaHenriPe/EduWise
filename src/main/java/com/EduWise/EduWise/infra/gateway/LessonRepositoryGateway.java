56f1829734c55154d4c5c115b3f2573b491cb38a
Gustavo Henrique Pereira Junqueira <guhhpj@gmail.com>
2025-10-03 15:35:55 -0300

    Feat: Implement CRUD operations for Lesson in LessonController and LessonRepositoryGateway

--- src/main/java/com/EduWise/EduWise/infra/gateway/LessonRepositoryGateway.java
+++ src/main/java/com/EduWise/EduWise/infra/gateway/LessonRepositoryGateway.java
@@ -1,35 +1,81 @@
 package com.EduWise.EduWise.infra.gateway;
 
 import com.EduWise.EduWise.core.domain.entities.Lesson;
+import com.EduWise.EduWise.core.domain.exceptions.LessonNotFoundException;
 import com.EduWise.EduWise.core.gateway.LessonGateway;
+import com.EduWise.EduWise.infra.mappers.lesson.LessonEntityMapper;
+import com.EduWise.EduWise.infra.persistence.entities.LessonEntity;
+import com.EduWise.EduWise.infra.persistence.entities.ModuleEntity;
+import com.EduWise.EduWise.infra.persistence.repositories.LessonRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Component
 public class LessonRepositoryGateway implements LessonGateway {
+
+    private final LessonRepository repository;
+    private final LessonEntityMapper mapper;
+    private final ModuleRepositoryGateway moduleRepositoryGateway;
+
+
     @Override
     public Lesson createLesson(Lesson lesson) {
-        return null;
+        ModuleEntity moduleEntity = moduleRepositoryGateway
+                .verifyExistingModule(lesson.moduleId());
+
+        LessonEntity lessonEntity = mapper.toEntity(lesson);
+
+        lessonEntity.setModuleEntity(moduleEntity);
+
+        LessonEntity savedLesson = repository.save(lessonEntity);
+
+        return mapper.toDomain(savedLesson);
     }
 
     @Override
     public List<Lesson> getAllLessons() {
-        return List.of();
+        return repository.findAll()
+                .stream()
+                .map(mapper::toDomain)
+                .toList();
     }
 
     @Override
     public Lesson getLessonById(Long id) {
-        return null;
+        LessonEntity lessonEntity = verifyLessonById(id);
+        return mapper.toDomain(lessonEntity);
     }
 
     @Override
     public Lesson updateLesson(Long id, Lesson lesson) {
-        return null;
+        LessonEntity existingLesson = verifyLessonById(id);
+
+        ModuleEntity moduleEntity = moduleRepositoryGateway
+                .verifyExistingModule(lesson.moduleId());
+
+        existingLesson.setTitle(lesson.title());
+        existingLesson.setContentType(lesson.contentType());
+        existingLesson.setVideoUrl(lesson.videoUrl());
+        existingLesson.setTextContent(lesson.textContent());
+        existingLesson.setModuleEntity(moduleEntity);
+
+        LessonEntity updatedLesson = repository.save(existingLesson);
+        return mapper.toDomain(updatedLesson);
     }
 
     @Override
     public void deleteLesson(Long id) {
+        LessonEntity existingLesson = verifyLessonById(id);
+        repository.delete(existingLesson);
     }
+
+    public LessonEntity verifyLessonById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new LessonNotFoundException(id));
+    }
+
 }
 
--- src/main/java/com/EduWise/EduWise/infra/presentation/LessonController.java
+++ src/main/java/com/EduWise/EduWise/infra/presentation/LessonController.java
@@ -1,9 +1,60 @@
 package com.EduWise.EduWise.infra.presentation;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.EduWise.EduWise.core.domain.entities.Lesson;
+import com.EduWise.EduWise.core.usecases.lesson.*;
+import com.EduWise.EduWise.infra.dtos.lesson.LessonRequest;
+import com.EduWise.EduWise.infra.dtos.lesson.LessonResponse;
+import com.EduWise.EduWise.infra.mappers.lesson.LessonRequestResponseMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("/lessons")
+@RequestMapping("/lessons/")
 public class LessonController {
+
+    private final CreateLessonUseCase createLessonUseCase;
+    private final GetAllLessonsUseCase getAllLessonsUseCase;
+    private final GetLessonByIdUseCase getLessonByIdUseCase;
+    private final UpdateLessonUseCase updateLessonUseCase;
+    private final DeleteLessonUseCase deleteLessonUseCase;
+    private final LessonRequestResponseMapper mapper;
+
+    @PostMapping()
+    public ResponseEntity<LessonResponse> createLesson(@Valid @RequestBody LessonRequest lessonRequest) {
+        Lesson savedLesson = createLessonUseCase.execute(mapper.toDomain(lessonRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.toResponse(savedLesson));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<LessonResponse>> getAllLessons() {
+        List<LessonResponse> lessons = getAllLessonsUseCase.execute()
+                .stream()
+                .map(mapper::toResponse)
+                .toList();
+        return ResponseEntity.ok(lessons);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Lesson> getLessonById(@PathVariable Long id) {
+        Lesson lesson = getLessonByIdUseCase.execute(id);
+        return ResponseEntity.ok(lesson);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<LessonResponse> updateLesson(@PathVariable Long id, @Valid @RequestBody LessonRequest lessonRequest) {
+        Lesson updatedLesson = updateLessonUseCase.execute(id, mapper.toDomain(lessonRequest));
+        return ResponseEntity.ok(mapper.toResponse(updatedLesson));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteLesson(@PathVariable Long id) {
+        deleteLessonUseCase.execute(id);
+        return ResponseEntity.noContent().build();
+    }
 }
